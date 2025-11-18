d4cdf13296cfc12a571bcd9d95ac239a088f4172
Benjamin Morrison <62651286+Banjomenny@users.noreply.github.com>
2025-11-18 17:57:36 -0500

    finalized the actual scanning part. actually it works

--- scanner.py
+++ scanner.py
@@ -63,13 +63,13 @@ def parse_arguments():
     :return: arguments
     '''
     #allows for nice CLI argument parsing
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='network scanner',usage='scans a given network for open ports')
     parser.add_argument("-a", "--address", action='store', dest='address', required=True,help="you can use CIDR notation or a something like 1.1.1.1-100. or specify single host")
     parser.add_argument('--mode', action='store',dest='portMode',choices=['common', 'range', 'all'], required=False, default='common',help='common is 1-1024, range you specify --startport and --endport and all is 1-65535')
     parser.add_argument('--start-port', type=int, action='store', dest='start', required=False, default=1,help='start port of range')
     parser.add_argument('--end-port', type=int, action='store', dest='end', required=False, default=1024,help='end port of range')
     parser.add_argument('-t', '--threads', type=int, action='store', dest='threads', required=False, default=1,help='number of threads')
-    parser.add_argument('-d', '--delay', type=float, action='store', dest='delay', required=False, default=1,help='delay in seconds')
+    parser.add_argument('-d', '--delay', type=float, action='store', dest='delay', required=False, default=0.3,help='delay in seconds')
     parser.add_argument('--display-only-open', action='store_true', dest='display_only_open', required=False, default=False, help='display only open port')
     parser.add_argument('--output-to-file', action='store_true', dest='output_to_file', required=False, default=False,help='output to file')
     parser.add_argument('--servicescan', action='store_true', dest='servicescan', required=False, default=False, help='service scan')
@@ -83,11 +83,11 @@ def getIPaddresses(address, threads):
     '''
     #allows for a range or cidr notation of ip addresses
     hosts = []
-    if '\\' in address:
+    if '/' in address:
         try:
             network = ipaddress.ip_network(address).hosts()
-            hosts = [str(network) for ip in network]
-            temp = temp = [[] for i in range(threads)]
+            hosts = [str(ip) for ip in network]
+            temp = [[] for i in range(threads)]
             for i in range(len(hosts)):
                 temp[i % threads].append(hosts[i])
             return temp
@@ -97,9 +97,11 @@ def getIPaddresses(address, threads):
         try:
             segments = address.split('.')
             hostRange = address.split('-')
-            for i in range(int(hostRange[1]), int(hostRange[0]) + 1 ):
-                hosts.append(f"{segments[0]}.{segments[1]}.{segments[2]}.{i}")
-            temp = [] * threads
+            for i in range(int(hostRange[0]), int(hostRange[1]) + 1 ):
+               if i < 255:
+                   sys.exit("invalid Octet")
+               hosts.append(f"{segments[0]}.{segments[1]}.{segments[2]}.{i}")
+            temp = [[] for i in range(threads)]
             for i in range(len(hosts)):
                 temp[i % threads].append(hosts[i])
             return temp
@@ -107,7 +109,7 @@ def getIPaddresses(address, threads):
             sys.exit("Invalid host range")
     else:
         try:
-            hosts.append(ipaddress.ip_address(address))
+            hosts.append(str(ipaddress.ip_address(address)))
             return hosts
         except:
             sys.exit("Invalid Host")
@@ -120,20 +122,22 @@ def scan_port(target, port):
     '''
     """Simple port scanner -- checks if the port is actually open"""
     try:
+        print(f"Scanning {target}... Port {port}")
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
+
+        sock.settimeout(.5)
         result = sock.connect_ex((target, port))
+
         sock.close()
 
         if result == 0:
-
             return "OPEN"
         else:
             return "CLOSED"
     except:
         return "ERROR"
 # takes in the info and runs the scan then it outputs to a group of all the threads results for post processing
-def busybeeIFMultipleHosts(delay, ports, hosts, groupedResults, index):
+def busybeeIFMultipleHosts(hosts, ports, delay, groupedResults, index):
     '''
     :param delay: delay between scans
     :param ports: ports to scan
@@ -158,7 +162,7 @@ def busybeeIFMultipleHosts(delay, ports, hosts, groupedResults, index):
 
 
 # only do one host. so only split ports and not hosts
-def busyBeeIFOneHost(hosts,delay, ports, groupedResults, index):
+def busyBeeIFOneHost(hosts, ports, delay, groupedResults, index):
     '''
     :param hosts: list of hosts to scan
     :param delay: delay between scanning
@@ -174,7 +178,7 @@ def busyBeeIFOneHost(hosts,delay, ports, groupedResults, index):
             local.append([hosts[0], port, common_ports_dict[port],state])
         else:
             state = scan_port(hosts[0], port)
-            local.append([hosts[0], port, 'UNKNOWN'])
+            local.append([hosts[0], port, 'UNKNOWN'], state)
         time.sleep(delay)
     groupedResults[index] = local
 
@@ -196,7 +200,7 @@ def getPorts(portMode, numberOfHosts, start, end, threads):
     '''
     listOfPorts = []
     if portMode == "common":
-        listOfPorts = common_ports_dict.keys()
+       listOfPorts = list(common_ports_dict.keys())
     elif portMode == "all":
         listOfPorts = list(range(1,65536))
     elif portMode == "range":
@@ -223,10 +227,10 @@ def main():
     try: int(args.threads)
     except ValueError: sys.exit('wrong value for threads: needs to be int')
 
-    try: int(args.start_port)
+    try: int(args.start)
     except ValueError: sys.exit('wrong value for startport: needs to be int')
 
-    try: int(args.end_port)
+    try: int(args.end)
     except ValueError: sys.exit('wrong value for endport: needs to be int')
 
 
@@ -240,20 +244,21 @@ def main():
     #create worker threads to then scan all ports. if 1 host is present splits up ports and if multiple hosts then splits up hosts
     for t in range (args.threads):
         if len(hosts) == 1:
-            thread = threading.Thread(target=busyBeeIFOneHost,args=(hosts,args.delay, ports[t], groupedResults, t))
+            thread = threading.Thread(target=busyBeeIFOneHost,args=(hosts, ports[t],args.delay, groupedResults, t))
             time.sleep(args.delay)
-            thread.start()
+            threads.append(thread)
 
         else:
-            thread = threading.Thread(target=busybeeIFMultipleHosts, args=(hosts[t], args.delay, ports, groupedResults, t))
+            thread = threading.Thread(target=busybeeIFMultipleHosts, args=(hosts[t], ports, args.delay, groupedResults, t))
             threads.append(thread)
-            thread.join()
+
 
 
     if args.threads != 1:
         for t in threads:
-            t.join()
             t.start()
+        for t in threads:
+            t.join()
 
     for group in groupedResults:
         for host, port, service, state in group:
@@ -262,6 +267,6 @@ def main():
 
 
 
-
+main()
 
 
